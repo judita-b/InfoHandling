cc5753b0dab9839a6dfcb9ba83edab0907cb8006
Judita <jud.bardzilauskaite@kvkedu.lt>
2021-07-02 12:30:55 +0300

    First change

--- src/main/java/com/epam/infohandling/model/Component.java
+++ src/main/java/com/epam/infohandling/model/Component.java
@@ -2,4 +2,9 @@ package com.epam.infohandling.model;
 
 public interface Component {
 
+    void add(Component component);
+    void remove(Component component);
+    void operation();
+    Object getChild(int index);
+
 }
--- src/main/java/com/epam/infohandling/model/Composite.java
+++ src/main/java/com/epam/infohandling/model/Composite.java
@@ -2,4 +2,23 @@ package com.epam.infohandling.model;
 
 public class Composite implements Component {
 
+    @Override
+    public void add(Component component) {
+
+    }
+
+    @Override
+    public void remove(Component component) {
+
+    }
+
+    @Override
+    public void operation() {
+
+    }
+
+    @Override
+    public Object getChild(int index) {
+        return null;
+    }
 }
