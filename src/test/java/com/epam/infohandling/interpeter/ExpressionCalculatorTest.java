7724ba5ceaa0b6203ee367e786c55246e0eabc09
Judita <jud.bardzilauskaite@kvkedu.lt>
2021-07-07 13:04:54 +0300

    Tried doing test, but not yet working.

--- src/test/java/com/epam/infohandling/interpeter/ExpressionCalculatorTest.java
+++ src/test/java/com/epam/infohandling/interpeter/ExpressionCalculatorTest.java
@@ -1,4 +1,38 @@
 package com.epam.infohandling.interpeter;
 
+import com.epam.infohandling.interpreter.ExpressionCalculator;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ExpressionCalculatorTest {
+    private Map<String, Integer> variables = new HashMap<String, Integer>();
+
+    public ExpressionCalculatorTest() {
+        variables.put("x", 7);
+    }
+
+    @Test
+    public void expressionCalculatorShouldCalculateWhenNumbersPositive() {
+        final String expression = "3 x +";
+
+        ExpressionCalculator expressionCalculator = new ExpressionCalculator();
+        Integer result = expressionCalculator.calculate(expression, variables);
+
+        Assert.assertNotNull(result);
+        Assert.assertEquals(10, result, 0.01);
+    }
+
+    @Test
+    public void expressionCalculatorTestWithNegativeNumbers() {
+        final String expression = "-3 x +";
+
+        ExpressionCalculator expressionCalculator = new ExpressionCalculator();
+        Integer result = expressionCalculator.calculate(expression, variables);
+
+        Assert.assertNotNull(result);
+        Assert.assertEquals(4, result, 0.01);
+    }
 }
